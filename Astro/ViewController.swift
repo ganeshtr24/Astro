165a2b973f71145aa4fd7b98939523ffc4318124
ganeshtr24 <ganeshtr24@gmail.com>
2023-03-20 21:54:11 +0530

    Initial workspace setup

---
+++ Astro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astro
+//
+//  Created by Ganesh TR on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
