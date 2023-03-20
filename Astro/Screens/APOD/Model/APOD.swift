730147978c6a8a58e3c16bc62527a3fa37a621b8
ganeshtr24 <ganeshtr24@gmail.com>
2023-03-20 22:29:12 +0530

    Base presentation Layer for APOD page

---
+++ Astro/Screens/APOD/Model/APOD.swift
@@ -0,0 +1,16 @@
+//
+//  APOD.swift
+//  Astro
+//
+//  Created by Ganesh TR on 20/03/23.
+//
+
+import Foundation
+
+struct APOD {
+    let copyright: String
+    let date: String
+    let explanation: String
+    let thumbnail_url: String
+    let title: String
+}
--- Astro/Screens/APOD/View/APODViewController.swift
+++ Astro/Screens/APOD/View/APODViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class APODViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
