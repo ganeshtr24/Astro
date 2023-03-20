1053c2b65b6abd7e2328b05f87e29f8ec6f0bed5
ganeshtr24 <ganeshtr24@gmail.com>
2023-03-20 22:32:22 +0530

    Add default network config

---
+++ Astro/Network/NetworkConfigurable.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkConfigurable.swift
+//  Astro
+//
+//  Created by Ganesh TR on 20/03/23.
+//
+
+import Foundation
+
+protocol NetworkConfigurable {
+    var baseURL: URL { get }
+    var headers: [String: String] { get }
+    var queryParameters: [String: String] { get }
+}
+
+public struct DefaultNetworkConfig: NetworkConfigurable {
+    public let baseURL: URL
+    public let headers: [String: String]
+    public let queryParameters: [String: String]
+    
+     public init(baseURL: URL,
+                 headers: [String: String] = [:],
+                 queryParameters: [String: String] = [:]) {
+        self.baseURL = baseURL
+        self.headers = headers
+        self.queryParameters = queryParameters
+    }
+}
